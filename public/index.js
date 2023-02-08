15c99f9490a2f62cc5c9f2ac4b49150f1acf08f2
github@bigfun.co.za <github@bigfun.co.za>
2023-02-08 20:44:23 +0200

    basic talking head

--- public/index.js
+++ public/index.js
@@ -4,12 +4,17 @@ let voices = [];
 let voice = null;
 let text = "";
 
+const avatar = new Avatar("bob");
+
 function populateVoiceList() {
   voices = synth.getVoices();
   const voicenames = voices.map(voice => voice.name);
   voicenames.forEach(v => console.log(v));
   //Yan
   voice = voices.find(v => v.name.indexOf("Ryan") > -1);
+  if (voice == null) {
+  voice = voices.find(v => v.name.indexOf("Guy") > -1);
+  }    
 }
 
 //populateVoiceList();
@@ -22,19 +27,33 @@ function speak(ntext) {
   setTimeout(() => {
     synth.cancel();
     const utterance = new SpeechSynthesisUtterance(text);
+
+    utterance.addEventListener("start", log);
+    utterance.addEventListener("end", log);
+
     utterance.addEventListener("boundary", animate);
     voice && (utterance.voice = voice);
     synth.speak(utterance);
   }, 1000);
 }
 
+function log(e) {
+console.log(e);
+}
+
 function animate(e) {
   const t = e.currentTarget.text;
   const word = t.substring(e.charIndex, e.charIndex + e.charLength);
+  avatar.setWord(word);
   console.log(word);
 }
 
-speak("Hello, I'm a text-to-speech system.");
+speak("morning");
+
+var button = document.getElementById("speak-button");
+button.addEventListener("click", () => {
+  speak("I'm a world famous avatar named guy");
+});
 
 
 
